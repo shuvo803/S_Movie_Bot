318c40f05207bdaf6ca6f0f217cb5953cb7f143b
shuvo803 <mdabulkhair675@gmail.com>
2025-06-11 14:44:54 +0600

    Update Script.py

--- Script.py
+++ Script.py
@@ -112,7 +112,7 @@ User - {}"""
 ⭐ Rating: {rating} / 10</b>
 """
 
-    FILE_CAPTION = """<b>{file_name}\n\n𝖩𝗈𝗂𝗇➥ 「......」</b>"""
+    FILE_CAPTION = """<b>{file_caption}\n\n𝖩𝗈𝗂𝗇➥ 「......」</b>"""
     
 
     ALRT_TXT = """ᴊᴀʟᴅɪ ʏᴇʜᴀ sᴇ ʜᴀᴛᴏ !"""
